d43977fe5fd59077cfea5244aefe503275d98429
Oleksandr Panchyshyn <alexandr.panchyshyn34@gmail.com>
2022-11-07 18:09:17 +0100

    test

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by user222882 on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func test() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
